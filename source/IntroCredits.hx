a9f0070644dcb4b43b7dc8f583f0384cf34a1f7f
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-07-10 10:48:48 -0300

    MP4 Handler

--- source/IntroCredits.hx
+++ source/IntroCredits.hx
@@ -14,8 +14,7 @@ class IntroCredits extends MusicBeatState
     override function create()
     {
         
-        var video:MP4Handler = new MP4Handler();
-        video.playMP4(Paths.video('introCREDITS'), new TitleState()); 
+        
 
         
         
